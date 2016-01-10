dc9745afaecf46f71b722bacce9ed3d866cfc008
Tazreen26 <tazreenkunjo@gmail.com>
2016-01-10 11:25:09 +0600

    1 button is added into CityEntryUI

--- CountryCityManagement/UI/CityEntryUI.aspx.designer.cs
+++ CountryCityManagement/UI/CityEntryUI.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace CountryCityManagement.UI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel cityInputPanel;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
